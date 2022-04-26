df91579ccea41a084b18288454ddd167933fa870
82109 <82109@LAPTOP-TSRH52U2>
2022-04-26 14:53:19 +0900

    추가

---
+++ src/test/java/com/rely/mapper/ReplyMapperTests.java
@@ -0,0 +1,24 @@
+package com.rely.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.reply.mapper.ReplyMapper;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+@Log4j
+public class ReplyMapperTests {
+
+	@Autowired ReplyMapper replymapper;
+	
+	@Test
+	public void testMapper() {
+		log.info(replymapper);
+	}
+}
